25851faa3785ce470cb288b00f6ebb7af19451b7
hyunuk <hus0316@ariatech.kr>
2021-07-05 15:27:18 +0900

    fetch data and loading spinner create

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import CardComponent from '../CardComponent';
-import { Container, Content, Icon } from 'native-base';
+import { Container, Content, Icon, Spinner } from 'native-base';
 
-export default function HomeTab() {
-    return (
-        <Container style={styles.container}>
-            <Content>
-                <CardComponent />
-            </Content>
-        </Container>
-    )
+export default class HomeTab extends Component {
+    state = {
+        feeds: [],
+        loading: true
+    }
+    componentDidMount() {
+        this.fetchFeeds().then(feeds => {
+            console.log(feeds);
+            this.setState({
+                feeds: feeds,
+                loading: false,
+            })
+        })
+    }
+    async fetchFeeds() {
+        const data = {
+            id: 1,
+            jsonrpc: "2.0",
+            method: "call",
+            params: [
+                "database_api",
+                "get_discussions_by_created",
+                [{ tag: "kr", limit: 20 }]
+            ]
+        };
+        const res = await fetch('https://api.steemit.com', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+        const res_1 = await res.json();
+        return res_1.result;
+    }
+    render() {
+        return (
+            <Container style={styles.container} >
+                <Content>
+                    {this.state.loading === true ?
+                        <Spinner style={styles.spiner} /> :
+                        (this.state.feeds.map((feed, index) => <CardComponent data={feed} key={index} />))
+                    }
+                </Content>
+            </Container>
+        )
+    }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
-    }
+    },
 })
--- Components/CardComponent.js
+++ Components/CardComponent.js
@@ -1,56 +1,60 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { Component } from "react";
 import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'native-base';
+import { ActivityIndicator, Image, Text } from "react-native";
 
-export default class CardComponent extends Component {
+export default class CardCompnent extends Component {
     render() {
+        const { data } = this.props; 
+        const { image } = JSON.parse(data.json_metadata); 
         return (
             <Card>
                 <CardItem>
                     <Left>
-                        <Thumbnail source={{ uri: 'https://steemitimages.com/u/anpigon/avatar' }} />
+                        <Thumbnail source={{ uri: `https://steemitimages.com/u/${data.author}/avatar` }} />
                         <Body>
-                            <Text>Anpigon</Text>
-                            <Text note>Jan 21, 2019</Text>
+                            <Text>{data.author}</Text>
+                            <Text note>{new Date(data.created).toDateString()}</Text>
                         </Body>
                     </Left>
                 </CardItem>
-                <CardItem cardBody>
-                    <Image
-                        source={{ uri: 'https://user-images.githubusercontent.com/3969643/51441420-b41f1c80-1d14-11e9-9f5d-af5cd3a6aaae.png' }}
-                        style={{ height: 200, width: null, flex: 1 }} />
+                {
+                    image && image.length ?
+                        <CardItem cardBody>
+                            <Image
+                                source={{ uri: image[0] }}
+                                style={{ height: 200, width: null, flex: 1 }} />
+                        </CardItem> : null
+                }
+                <CardItem style={{ height: 20 }}>
+                    <Text>{data.active_votes.length} likes</Text>
+                </CardItem>
+                <CardItem>
+                    <Text style={{ fontWeight: '900' }}>{data.title}</Text>
+                </CardItem>
+                <CardItem>
+                    <Text>
+                        {data.body.replace(/\n/g, ' ').slice(0, 200)}
+                    </Text>
                 </CardItem>
                 <CardItem style={{ height: 45 }}>
                     <Left>
                         <Button transparent>
-                            <Icon name='ios-heart' style={{ color: 'black' }} />
+                            <Icon name='ios-heart' style={{ color: 'black', marginRight: 5 }} />
+                            <Text>{data.active_votes.length}</Text>
                         </Button>
                         <Button transparent>
-                            <Icon name='ios-chatbubbles' style={{ color: 'black' }} />
+                            <Icon name='ios-chatbubbles' style={{ color: 'black', marginRight: 5 }} />
+                            <Text>{data.children}</Text>
                         </Button>
                         <Button transparent>
                             <Icon name='ios-send' style={{ color: 'black' }} />
                         </Button>
                     </Left>
-                </CardItem>
-                <CardItem style={{ height: 20 }}>
-                    <Text>101 likes</Text>
-                </CardItem>
-                <CardItem>
-                    <Text>
-                        <Text style={{ fontWeight: '900' }}>Anpigon</Text>
-                        이번에는 리액트 네이티브(React Native)로 인스타그램 UI을 구현하는 포스팅입니다. 다른 앱을 따라 만들어 보는 것은 굉장히 재미있습니다. 구글에서 인스타그램 클론 코딩 강의를 찾아보니, 다른 개발자들이 올린 동영상 강의를 몇 개 찾을 수 있었습니다.
-                    </Text>
+                    <Right>
+                        <Text>{data.pending_payout_value}</Text>
+                    </Right>
                 </CardItem>
             </Card>
         );
     }
 }
-
-const styles = StyleSheet.create({
-
-    profileImg: {
-        width: 30
-    },
-
-})
